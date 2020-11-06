6a22575270d0cb1710abc04ec58e1a42d42be1d3
zhangfan <zhangfan@uniev.com>
2020-11-06 17:12:24 +0800

    example

---
+++ example/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:popular_alert/popular_alert.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: TempPage(),
+    );
+  }
+}
+
+class TempPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Demo'),
+      ),
+      body: Center(child: RaisedButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return PopularAlert(
+                  title: 'title',
+                  content: 'content',
+                  actions: [
+                    FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Cancel',
+                          style: TextStyle(color: Colors.blue),
+                        )),
+                    FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Ok',
+                          style: TextStyle(color: Colors.red),
+                        )),
+                  ],
+                );
+              });
+        },
+        child: Text('show'),
+      ),),
+    );
+  }
+}
